569ab48c93db4282377cf025684e8fed6446f6f8
Antonio Blandin <antonio.blandin25@gmail.com>
2019-05-19 01:39:22 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,6 +25,6 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  shipping_manifest["muskets"] = "2"
+  shipping_manifest["muskets"] = 2
   puts shipping_manifest
 end
